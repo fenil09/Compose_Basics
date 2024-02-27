38d2e5751244a43d4b2e236a2ac869afc99764f4
fenil09 <backsystem9909@gmail.com>
2024-02-27 13:20:18 -0800

    Creating an pattern using the borders which are applied using modifiers.

--- app/src/main/java/com/example/compose_basics/MainActivity.kt
+++ app/src/main/java/com/example/compose_basics/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_basics.ui.theme.Compose_BasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,13 +39,20 @@ class MainActivity : ComponentActivity() {
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight(0.5f)
-                .background(Color.Green),
+                .background(Color.Green)
+                .border(5.dp,Color.Magenta)
+                .padding(5.dp)
+                .border(10.dp,Color.Black)
+                .padding(10.dp)
+                .border(10.dp,Color.Blue)
+                .padding(10.dp)
+                .border(12.dp,Color.Yellow)
+                .padding(12.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
+            verticalArrangement = Arrangement.Center
+
         ) {
-            Text(text = "Fenil")
-            Text(text = "Ahir")
-            Text(text = "Bardoli")
+            Text(text = "Hello", color = Color.Black)
         }
     }
 }
